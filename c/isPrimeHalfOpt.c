0ec6a99129d7df7422511cca5f1446f3e7f9cc3d
Soldy <4786022+Soldy@users.noreply.github.com>
2020-08-17 21:38:10 +0100

    c + rust prime test
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ c/isPrimeHalfOpt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+typedef enum { false, true } bool;
+int primes =0;
+
+bool isPrime(int number){
+   int i;
+   for (i = 2; i < sqrt(number)+1; i++ ){
+       if (number % i == 0){
+           return false;
+       }
+   }
+   return true;
+}
+
+int main(){
+    int k;
+    for (k = 2; k < 200000; k++){
+        if (isPrime(k) == true){
+           primes++;
+        }
+     }
+     printf("\n%d\n", primes);
+}
--- rust/isPrimeHalfOpt.rs
+++ rust/isPrimeHalfOpt.rs
@@ -1,5 +1,5 @@
 fn isprime(n: u32) -> bool {
-    let limit = n;
+    let limit = ((n as f64).sqrt() as u32)+1;
     for i in 2..=limit {
         if n % i == 0 {
              return false;
