98684aef51a69290aeab82670a0ffbe1a0310916
Soldy <4786022+Soldy@users.noreply.github.com>
2020-08-17 21:44:26 +0100

    javascript
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ javascript/isPrimeHalfOpt.js
@@ -0,0 +1,15 @@
+
+let primes = 0;
+
+function isPrime(number){
+    for (let i = 2; i < Math.sqrt(number)+1; i++)
+        if (number % i == 0)
+            return false;
+    return true;
+}
+for (let v = 2; v < 200000; v++)
+    if (isPrime(v))
+       primes++;
+
+console.log(primes.toString()+"\n");
+
