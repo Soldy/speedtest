a883816857fceebee5dcea64375292383f2e4e1f
Soldy <4786022+Soldy@users.noreply.github.com>
2020-10-04 18:01:07 +0100

    php most opt with class
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ php/isPrimeMostOptClass.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class prime {
+      private $realPrimes = [2,3,5];
+      private function isPrime($number){
+           for ($i = 0; $this->realPrimes[$i] < sqrt($number)+1; $i++ )
+               if ($number % $this->realPrimes[$i] == 0)
+                    return false;
+               return true;
+     }
+     public function __construct (){
+         for ($k = 6 ; $k < 2000000; $k++)
+             if ($this->isPrime($k))
+                 $this->realPrimes[]=$k;
+         echo "\n".count($this->realPrimes)."\n";
+     }
+}
+
+
+(new prime());
