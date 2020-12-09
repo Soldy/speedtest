7d0b4090fc6c380ff1c4f1be27a7518da7ac7f7a
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-09 16:22:58 +0000

    php gnome sort
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ php/libs/sort/gnome.php
@@ -0,0 +1,19 @@
+<?php
+
+final class GnomeSort extends SortAbstract implements SortInterface
+{
+    use SwapTrait {swap as private; }
+    protected function doIt ()
+    {
+        $i = 1;
+        while ($this->length > $i){
+             if ($this->store[$i-1] > $this->store[$i]){
+                 $this->swap($i, $i-1);
+             } else
+                 $i++;
+        }
+    }
+}
+
+
+
