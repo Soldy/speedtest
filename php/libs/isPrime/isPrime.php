c777bc50924f02daf929117e6cff2e4dcb90495f
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-12 14:45:54 +0000

    shine ???
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- php/libs/isPrime/isPrime.php
+++ php/libs/isPrime/isPrime.php
@@ -1,13 +1,18 @@
 <?php
 $primes = 0;
-function isPrime($number){
-   for ($i = 2; $i < $number; $i++ )
-       if ($number % $i == 0)
-           return false;
-   return true;
+function isPrime($number)
+{
+    for ($i = 2; $i < $number; $i++ ) {
+        if ($number % $i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-for ($k = 0; $k < 200000; $k++)
-   if (isPrime($k))
-       $primes++;
+for ($k = 0; $k < 200000; $k++) {
+    if (isPrime($k)) {
+        $primes++;
+    }
+}
 echo "\n".$primes."\n";
--- php/libs/isPrime/isPrimeMostOpt.php
+++ php/libs/isPrime/isPrimeMostOpt.php
@@ -2,14 +2,19 @@
 
 $realPrimes = [2,3,5,7];
 
-function isPrime($number, $realPrimes){
-   for ($i = 0; $realPrimes[$i] < sqrt($number)+1; $i++ )
-       if ($number % $realPrimes[$i] == 0)
-           return false;
-   return true;
+function isPrime($number, $realPrimes)
+{
+    for ($i = 0; $realPrimes[$i] < sqrt($number)+1; $i++ ) {
+        if ($number % $realPrimes[$i] == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
-for ($k =8; $k < 20000000; $k++)
-   if (isPrime($k, $realPrimes))
-       $realPrimes[]=$k;
+for ($k =8; $k < 20000000; $k++) {
+    if (isPrime($k, $realPrimes)) {
+        $realPrimes[]=$k;
+    }
+}
 echo "\n".count($realPrimes)."\n";
