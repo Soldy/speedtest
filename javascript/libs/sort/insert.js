2c0bfb880299839b2ce97ddd32a5735a38f88d81
Soldy <4786022+Soldy@users.noreply.github.com>
2020-12-13 13:32:47 +0000

    insert sort
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ javascript/libs/sort/insert.js
@@ -0,0 +1,26 @@
+const insertSortBase = function(){
+    this.sort = function(inArr){
+        store = [...inArr];
+        length = inArr.length;
+        sort();
+        return store;
+    }
+    let length;
+    let store;
+    const swap = function (A, B) {
+        let temp = store[A];
+        store[A] = store[B];
+        store[B] = temp;
+    }
+    const sort = function(){
+        for (let i = 1 ; length > i ; i++){
+            let x = store[i];
+            let j = i -1;
+            while ( (j >= 0) && (store[j]>x)){
+                svap(j-1, j);
+                j=j-1;
+            }
+        }
+    }
+}
+
