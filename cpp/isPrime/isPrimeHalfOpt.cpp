09bc5469ab4cf8df15325fdd6012b5f645721799
Soldy <4786022+Soldy@users.noreply.github.com>
2025-07-19 18:09:18 +0100

    cpp prime calculations
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

---
+++ cpp/isPrime/isPrimeHalfOpt.cpp
@@ -0,0 +1,23 @@
+#include <cmath>
+#include <iostream>
+uint32_t primes =1;
+
+bool isPrime(uint32_t number){
+   uint32_t i;
+   for (i = 2; i < std::sqrt(number); i++ ){
+       if (number % i == 0){
+           return false;
+       }
+   }
+   return true;
+}
+
+int main(){
+    uint32_t k;
+    for (k = 3; k < 200000; k++){
+        if (isPrime(k)){
+           primes++;
+        }
+     }
+     std::cout << primes << std::endl;
+}
---
+++ cpp/isPrime/isPrimeMostOpt.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+#include <iostream>
+#include <vector>
+uint32_t primes =0;
+std::vector<int32_t> realPrimes;
+
+bool isPrime(int number){
+   int i;
+   float limit = sqrt(number)+1;
+   for (i = 0; realPrimes[i] < limit; i++){
+       if (number % realPrimes[i] == 0){
+           return false;
+       }
+   }
+   return true;
+}
+
+int main(){
+    uint32_t k;
+    realPrimes.push_back(2);
+    primes++;
+    realPrimes.push_back(3);
+    primes++;
+    realPrimes.push_back(5);
+    primes++;
+    for (k = 6; k < 200000; k++){
+        if (isPrime(k)){
+           realPrimes.push_back(k);
+           primes++;
+        }
+     }
+     std::cout << primes << std::endl;
+}
