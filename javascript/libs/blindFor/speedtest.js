37dbe0ba8c778079fd4587374706da426e2a29c9
Soldy <4786022+Soldy@users.noreply.github.com>
2025-07-20 09:54:12 +0100

    javascript blind for clean up
    Signed-off-by: Soldy <4786022+Soldy@users.noreply.github.com>

--- javascript/libs/blindFor/speedtest.js
+++ javascript/libs/blindFor/speedtest.js
@@ -1,13 +1,13 @@
-var i = 0;
-    for (ia = 1 ; ia < 90; ia++){
-        for (ib = 1 ; ib < 89; ib++){
-            for (ic = 1 ; ic < 88; ic++){
-                  for (id = 1 ; id < 87; id++){
-                      for (ie = 1 ; ie < 86; ie++){
-                               i++;
+let i = 0;
+    for (let ia = 1 ; ia < 90; ia++){
+        for (let ib = 1 ; ib < 89; ib++){
+            for (let ic = 1 ; ic < 88; ic++){
+                  for (let id = 1 ; id < 87; id++){
+                      for (let ie = 1 ; ie < 86; ie++){
+                          i++;
                     }
                 }
             }
         }
     }
-console.log("osszesen:"+ i.toString());
+console.log(i.toString());
